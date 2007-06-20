0169ef8901c544e9ba5b75dd387adbb1665e52e8
Mikhail Subach <1397738+Scorpibear@users.noreply.github.com>
2007-06-20 12:17:59 +0000

    Fix bug [ 1711760 ] Exception after entering string in Start column of Time Log
    Autocomplete fix

--- LazyCure.UI/Properties/AssemblyInfo.cs
+++ LazyCure.UI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("LifeIdea.org")]
 [assembly: AssemblyProduct("LazyCure.UI")]
-[assembly: AssemblyCopyright("Copyright © LifeIdea.org 2003-2007")]
+[assembly: AssemblyCopyright("Free Open Source under GNU Lesser General Public License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("3.1.0.10")]
-[assembly: AssemblyFileVersion("3.1.0.10")]
+[assembly: AssemblyVersion("3.1.0.11")]
+[assembly: AssemblyFileVersion("3.1.0.11")]
