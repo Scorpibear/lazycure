49c81a842dea966ec815cf43ccead3926b454a06
Scorpibear <Scorpibear@ce9dbcaf-9ae4-4adf-a414-be565aece6b4>
2007-09-17 02:25:13 +0000

    LazyCure 3.1.1

--- trunk/LazyCure.Core/Properties/AssemblyInfo.cs
+++ trunk/LazyCure.Core/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.1.0.23")]
-[assembly: AssemblyFileVersion("3.1.0.23")]
+[assembly: AssemblyVersion("3.1.1.0")]
+[assembly: AssemblyFileVersion("3.1.1.0")]
