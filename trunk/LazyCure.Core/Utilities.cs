b90ba402032d2c194fcd9311a3bc10a8a8ec262d
scorpibear <scorpibear@ce9dbcaf-9ae4-4adf-a414-be565aece6b4>
2008-03-26 21:24:14 +0000

    BugFixed: [ 1926453 ] 'Working' property of task is not saved.

--- trunk/LazyCure.Core/Utilities.cs
+++ trunk/LazyCure.Core/Utilities.cs
@@ -26,5 +26,24 @@ namespace LifeIdea.LazyCure.Core
             else
                 return true;
         }
+
+        public static string BoolToString(bool value)
+        {
+            return value.ToString().ToLower();
+        }
+
+        /// <summary>
+        /// Convert string to boolean value. If string does not represent bool, returns true.
+        /// </summary>
+        /// <param name="value">string representation of boolean</param>
+        /// <returns>boolean value</returns>
+        public static bool StringToBool(string value)
+        {
+            bool result;
+            if (bool.TryParse(value,out result))
+                return result;
+            else
+                return true;
+        }
     }
 }
