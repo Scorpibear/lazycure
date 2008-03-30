f4e9e7d6f9b6cfc8d5d3f1fb91625ac0b7a7461c
Mikhail Subach <1397738+Scorpibear@users.noreply.github.com>
2008-03-30 13:45:13 +0000

    LazyCure 3.1.8. Task Manager caption was changed. readme and whatsnew was updated.

--- LazyCure/Properties/AssemblyInfo.cs
+++ LazyCure/Properties/AssemblyInfo.cs
@@ -40,5 +40,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.1.7.0")]
-[assembly: AssemblyFileVersion("3.1.7.0")]
+[assembly: AssemblyVersion("3.1.8.0")]
+[assembly: AssemblyFileVersion("3.1.8.0")]
