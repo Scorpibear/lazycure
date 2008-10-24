4dee909939bc559acf2b82306d6e0199772ecd7e
Mikhail Subach <1397738+Scorpibear@users.noreply.github.com>
2008-10-24 19:41:35 +0000

    Bug fix: [1711742] LazyCure windows does not save positions after restarting.

---
+++ LazyCure.UI/MainBase.cs
@@ -0,0 +1,22 @@
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace LifeIdea.LazyCure.UI
+{
+    public class MainBase: Form
+    {
+        public void SetLocation(Point location)
+        {
+            Size desktopSize = Screen.PrimaryScreen.WorkingArea.Size;
+            if (location.X < 0)
+                location.X = 0;
+            if (location.X > desktopSize.Width - Width)
+                location.X = desktopSize.Width - Width;
+            if (location.Y < 0)
+                location.Y = 0;
+            if (location.Y > desktopSize.Height - Height)
+                location.Y = desktopSize.Height - Height;
+            Location = location;
+        }
+    }
+}
