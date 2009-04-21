1b4adb183ec7af3bae8570e6c5a5859f543a9cfc
scorpibear <scorpibear@ce9dbcaf-9ae4-4adf-a414-be565aece6b4>
2009-04-21 07:15:26 +0000

    Bug fix: [2660668] Post to twitter feature is not working

--- trunk/LazyCure/Properties/AssemblyInfo.cs
+++ trunk/LazyCure/Properties/AssemblyInfo.cs
@@ -42,5 +42,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("3.6.0.0")]
-[assembly: AssemblyFileVersion("3.6.0.0")]
+[assembly: AssemblyVersion("3.7.0.0")]
+[assembly: AssemblyFileVersion("3.7.0.0")]
