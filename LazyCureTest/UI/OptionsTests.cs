692c419b395aaad60bf127d5c5faff6dcecaadb5
Mikhail Subach <1397738+Scorpibear@users.noreply.github.com>
2010-11-10 22:02:24 +0000

    Stub for OptionsTests has been added.

---
+++ LazyCureTest/UI/OptionsTests.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+using NMock2;
+using LifeIdea.LazyCure.Interfaces;
+
+namespace LifeIdea.LazyCure.UI
+{
+    [TestFixture]
+    public class OptionsTests: Mockery
+    {
+
+        
+
+    }
+}
