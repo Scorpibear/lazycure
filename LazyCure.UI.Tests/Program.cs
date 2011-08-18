93cf6c5b1d72751dab6bf8d9e0d0bfce746af33a
Mikhail Subach <1397738+Scorpibear@users.noreply.github.com>
2011-08-18 14:47:10 +0000

    Unit tests were split on separate projects.

---
+++ LazyCure.UI.Tests/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LazyCure.UI.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
