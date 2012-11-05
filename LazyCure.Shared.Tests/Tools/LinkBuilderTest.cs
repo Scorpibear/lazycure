e18f4652bfa9245aa5aa4bfa01d37e2dd5fd782a
Mikhail Subach <1397738+Scorpibear@users.noreply.github.com>
2012-11-05 04:06:06 +0000

    Bug fix: [3489996] file ru-RU/LazyCure.html not found

---
+++ LazyCure.Shared.Tests/Tools/LinkBuilderTest.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+
+namespace LifeIdea.LazyCure.Shared.Tools
+{
+    [TestFixture]
+    public class LinkBuilderTest
+    {
+        [Test]
+        public void LocalizationFolderNameHas2Letters()
+        {
+            Assert.AreEqual(2, LinkBuilder.LocalizationFolder.Length);
+        }
+    }
+}
